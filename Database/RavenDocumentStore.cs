9cc625a4c93b17a9e4f3ad4f29737be5f4a0ecc7
ThePrograminator <christian_skyder@hotmail.com>
2019-11-26 14:24:16 +0100

    - Setup RavenDB Boilerplate - Created Simple working version of Event

---
+++ Database/RavenDocumentStore.cs
@@ -0,0 +1,25 @@
+﻿using Raven.Client.Documents;
+using System;
+
+namespace Database
+{
+    public class RavenDocumentStore
+    {
+        //
+        private static Lazy<IDocumentStore> store = new Lazy<IDocumentStore>(CreateStore);
+
+        public static IDocumentStore Store => store.Value;
+
+        private static IDocumentStore CreateStore()
+        {
+            string ravenDBIP = ConfigurationManager.AppSettings["RavenDBEndPoint"];
+            IDocumentStore store = new DocumentStore()
+            {
+                Urls = new[] { ravenDBIP },
+                Database = "Scores"
+            }.Initialize();
+
+            return store;
+        }
+    }
+}
---
+++ EventAPI/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EventAPI.Model;
+using EventAPI.RavenDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Raven.Client.Documents.Session;
+
+namespace EventAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Event")]
+    public class EventController : ControllerBase
+    {
+        private readonly ILogger<EventController> _logger;
+
+        public EventController(ILogger<EventController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/Event
+        public IEnumerable<Event> Get()
+        {
+            using (IDocumentSession session = RavenDocumentStore.Store.OpenSession())  // Open a session for a default 'Database'
+            {
+                //EnsureDatabaseExists.DatabaseExists(RavenDocumentStore.Store, "Scores");
+                List<Event> events = session
+                    .Query<Event>()
+                    .ToList();
+                return events;
+            }
+        }
+    }
+}
